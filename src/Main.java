b07b76af2acc3ebcfe2cca54eddbd4821d592fb5
SonglinWang1111 <songlin.wang@uzh.ch>
2022-12-14 19:24:26 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Testing!");
-        System.out.println("I am Software Destructor");
+        String string = "I am Software Destructor";
+        System.out.println(string);
     }
 }
