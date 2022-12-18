247e8bd94fd24c7bd79492c4832fc17bd8d91dc4
SonglinWang1111 <songlin.wang@uzh.ch>
2022-12-18 04:17:08 +0100

    1

--- src/Grid.java
+++
@@ -1,9 +0,0 @@
-public class Grid {
-
-    public Grid(int rows, int cols) throws InvalidSize{
-        if (rows < 10 || rows > 100 || cols < 10 || cols > 100){
-            throw new InvalidSize();
-        }
-        System.out.println(rows + "," + cols);
-    }
-}
--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GameControl game = new GameControl();
-        game.setUpBoard();
-    }
-}
