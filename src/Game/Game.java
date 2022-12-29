fad4be4b846bb3ffef519c146489384324fcb972
Songlin Wang <Songlin.Wang@uzh.ch>
2022-12-29 03:06:22 +0100

    Strategy

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -5,8 +5,6 @@ import exceptions.InvalidNewCell;
 import exceptions.InvalidSize;
 import board.GameBoard;
 import player.Player;
-import player.PlayeruseColor;
-import player.PlayeruseSymbol;
 
 import static grid.Grid.GridStatus;
 
@@ -15,7 +13,7 @@ import java.util.*;
 public class Game {
     private final int numPlayers;
     private final boolean colorornot;
-    private final List<Player> players = new ArrayList<>();
+    private List<Player> players = new ArrayList<>();
     private int rows;
     private int cols;
     private GameBoard aGameBoard;
@@ -28,28 +26,12 @@ public class Game {
     public void setUpPlayers(){
         System.out.println("Rules for inputting names: Please input your first name, middle names(optional) and last name, using upper-case for first letters. Please use a whitespace to separate them.");
         if (!this.colorornot) {
-            for (int i = 0; i < this.numPlayers; i++) {
-                String player_name = getPlayerName(i + 1);
-                String player_symbol = getPlayerSymbol();
-                this.players.add(new PlayeruseSymbol(player_name, player_symbol));
-            }
-            if (Objects.equals(this.players.get(0).getSymbol(), this.players.get(1).getSymbol())){
-                System.out.println("The symbol of 2 players could not be the same!");
-                this.players.clear();
-                setUpPlayers();
-            }
+            GameContext gameContext = new GameContext(new GameuseSymbol());
+            this.players = gameContext.SetupPlayers(this.numPlayers);
         }
         else{
-            for (int i = 0; i < this.numPlayers; i++) {
-                String player_name = getPlayerName(i + 1);
-                String player_color = getPlayerColor();
-                this.players.add(new PlayeruseColor(player_name, player_color));
-            }
-            if (Objects.equals(this.players.get(0).getColor(), this.players.get(1).getColor())){
-                System.out.println("The color of 2 players could not be the same!");
-                this.players.clear();
-                setUpPlayers();
-            }
+            GameContext gameContext = new GameContext(new GameuseColor());
+            this.players = gameContext.SetupPlayers(this.numPlayers);
         }
 
         if (Objects.equals(this.players.get(0).getName(), this.players.get(1).getName())){
@@ -73,49 +55,6 @@ public class Game {
 
         // symbol: 0, color: 1
         return SymbolorColor.equals("color");
-    }
-
-    public String getPlayerName(int num){
-        Scanner scanner = new Scanner(System.in);
-
-        // Read the name of players
-        System.out.print("please input name " + num + ": ");
-        String playerName = scanner.nextLine();
-        while(!playerName.matches("([A-Z][a-z]*\\s)+[A-Z][a-z]*")){
-            System.out.print("Please input a valid name according to the rules: ");
-            playerName = scanner.nextLine();
-        }
-
-        return playerName;
-    }
-
-    public String getPlayerSymbol() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Please input a single sign as your symbol (not letters or numbers): ");
-        String playerSymbol = scanner.nextLine();
-        while (!playerSymbol.matches("\\W")) {
-            System.out.print("Please type in a correct symbol: ");
-            playerSymbol = scanner.nextLine();
-        }
-
-        return playerSymbol;
-    }
-
-    public String getPlayerColor(){
-        Scanner scanner = new Scanner(System.in);
-        List<String> colorList = new ArrayList<>(6);
-        colorList.add("red"); colorList.add("green"); colorList.add("yellow");
-        colorList.add("blue"); colorList.add("cran"); colorList.add("purple");
-
-        System.out.print("Please input a color from the color list: 'red', 'green', 'yellow', 'blue', 'cran', 'purple': ");
-        String playerColor = scanner.nextLine();
-        while (!colorList.contains(playerColor)) {
-            System.out.print("Please type in a correct color: ");
-            playerColor = scanner.nextLine();
-        }
-
-        return playerColor;
     }
 
     public void setUpBoard(){
---
+++ src/Game/GameInterface.java
@@ -0,0 +1,9 @@
+package Game;
+
+import player.Player;
+
+import java.util.List;
+
+public interface GameInterface {
+    List<Player> SetupPlayers (int num);
+}
