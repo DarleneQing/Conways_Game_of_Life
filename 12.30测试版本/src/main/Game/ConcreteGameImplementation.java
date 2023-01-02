88e83d0ba0a87a83da9e0bf47a695c219d557522
MO ZHANG <108749730+jason99696@users.noreply.github.com>
2023-01-02 12:24:36 +0100

    Update ConcreteGameImplementation.java

--- 12.30测试版本/src/main/Game/ConcreteGameImplementation.java
+++ 12.30测试版本/src/main/Game/ConcreteGameImplementation.java
@@ -8,7 +8,7 @@ public class ConcreteGameImplementation implements GameInterface {
 
     @Override
     public List<Player> SetupPlayers(int num) {
-        // TODO: Implement this method to set up the players for the game
+    
         return null;
     }
 }
